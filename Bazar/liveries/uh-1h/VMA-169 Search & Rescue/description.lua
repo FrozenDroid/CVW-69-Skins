87fbf85b3a5890b604f8b60b24497d95813e46cb
Dominic M <dominic@infiniteaspects.co.uk>
2020-04-10 16:40:17 +0100

    Update 20.04.01

---
+++ Bazar/liveries/uh-1h/VMA-169 Search & Rescue/description.lua
@@ -0,0 +1,34 @@
+livery = {
+	{"civ_uh1_main_dam", 0, "uh1", false};
+	{"civ_uh1_main_dam", 2, "uh1_spec_37th.bmp", false};
+        {"civ_uh1_tail_dam", 0, "uh1_tail", false};
+	{"civ_uh1_tail_dam", 2, "uh1_tail_spec_37th.bmp", false};
+        {"civ_uh1_main", 0, "uh1", false};
+	{"civ_uh1_main", 2, "uh1_spec_37th.bmp", false};
+        {"civ_uh1_tail", 0, "uh1_tail", false};	
+        {"civ_uh1_tail", 2, "uh1_tail_spec_37th.bmp", false};
+
+	{"uh1_main_dam", 0, "uh1", false};
+	{"uh1_main_dam", 2, "uh1_spec.bmp",false};
+        {"uh1_tail_dam", 0, "uh1_tail", false};
+	{"uh1_tail_dam", 2, "uh1_tail_spec.bmp",false};
+        {"uh1_main", 0, "uh1", false};
+	{"uh1_main", 2, "uh1_spec.bmp",false};
+        {"uh1_tail", 0, "uh1_tail", false};	
+	{"uh1_tail", 2, "uh1_tail_spec.bmp",false};
+        {"uh1_weapon", 0, "uh1_weapon_blk", true};
+	{"pilot_UH1_01", 0, "pilot_uh1_us", false};
+	{"pilot_UH1_02", 0, "pilot_uh1_us", false};
+	{"pilot_UH1_helmet", 0, "pilot_UH1_helmet_us", false};
+	{"pilot_UH1_patch", 0, "pilot_UH1_patch_92nd_gremlins", false};
+	{"uh1_cov", 0, "uh1-us_cover", false};
+        {"uh1_cov", 2, "uh1_cover_spec", false};
+	{"UH1-cpt-door", 0 ,"UH1_door_c_gray",false};
+	{"UH1-cpt_karkas", 0 ,"uh1_karkas_c_gray",false};
+	{"UH1-cpt_karkas", 2 ,"UH1_karkas_s",false};
+	{"UH1-cpt_panel", 0 ,"uh1_panel_c_gray",true};
+	{"UH1-cpt_central_box", 0 ,"uh1_boxs_c_gray",true};
+	{"UH1-cpt_dev2", 0 ,"UH1_dev_2",true};
+}
+name = "VMA-169 CSAR"
+countries = {"USA", "RUS", "FRA", "UKR", "SPN", "NETH", "TUR", "BEL", "GER", "NOR", "CAN", "DEN", "UK", "GRG", "ISR", "ABH", "RSO"}
---
+++ Bazar/liveries/uh-1h/VMA-169 Two Tone/description.lua
@@ -0,0 +1,34 @@
+livery = {
+	{"civ_uh1_main_dam", 0, "civ_uh1_37th.bmp", false};
+	{"civ_uh1_main_dam", 2, "civ_uh1_spec_37th.bmp", false};
+	{"civ_uh1_tail_dam", 0, "civ_uh1_tail_37th.bmp", false};
+	{"civ_uh1_tail_dam", 2, "civ_uh1_tail_spec_37th.bmp", false};
+	{"civ_uh1_main", 0, "civ_uh1_37th.bmp", false};
+	{"civ_uh1_main", 2, "civ_uh1_spec_37th.bmp", false};
+	{"civ_uh1_tail", 0, "civ_uh1_tail_37th.bmp", false};
+	{"civ_uh1_tail", 2, "civ_uh1_tail_spec_37th.bmp", false};	
+
+	{"uh1_main_dam", 0, "uh1_37th.bmp", false};
+	{"uh1_main_dam", 2, "uh1_spec_37th.bmp", false};
+	{"uh1_tail_dam", 0, "uh1_tail_37th.bmp", false};
+	{"uh1_tail_dam", 2, "uh1_tail_spec_37th.bmp", false};
+	{"uh1_main", 0, "uh1_37th.bmp", false};
+	{"uh1_main", 2, "uh1_spec_37th.bmp", false};
+	{"uh1_tail", 0, "uh1_tail_37th.bmp", false};
+	{"uh1_tail", 2, "uh1_tail_spec_37th.bmp", false};	
+	{"uh1_weapon", 0, "uh1_weapon_blk", true};
+	{"pilot_UH1_01", 0, "pilot_uh1_us", false};
+	{"pilot_UH1_02", 0, "pilot_uh1_us", false};
+	{"pilot_UH1_helmet", 0, "pilot_UH1_helmet_37th.bmp", false};
+	{"pilot_UH1_patch", 0, "pilot_UH1_patch_37th.tga", false};	
+	{"uh1_cov", 0, "uh1_cover_37th.bmp", false};
+	{"uh1_cov", 2, "uh1_cover_spec_37th.bmp", false};
+	{"UH1-cpt-door", 0 ,"UH1_door_c_gray",false};
+	{"UH1-cpt_karkas", 0 ,"uh1_karkas_c_gray",false};
+	{"UH1-cpt_karkas", 2 ,"UH1_karkas_s",false};
+	{"UH1-cpt_panel", 0 ,"uh1_panel_c_gray",true};
+	{"UH1-cpt_central_box", 0 ,"uh1_boxs_c_gray",true};
+	{"UH1-cpt_dev2", 0 ,"UH1_dev_2",true};
+}
+name = "VMA-169 'Orca'"
+countries = {"USA", "RUS", "FRA", "UKR", "SPN", "NETH", "TUR", "BEL", "GER", "NOR", "CAN", "DEN", "UK", "GRG", "ISR", "ABH", "RSO"}
