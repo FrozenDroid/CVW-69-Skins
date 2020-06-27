1bf19c5afd398187be1c0de379e0a47efad024b9
dsrtfx <dsrtfx@users.noreply.github.com>
2020-06-26 22:24:42 -0500

    Update to 20.5.22

---
+++ Bazar/liveries/e-2c/E-2D VAW-69/description.lua
@@ -0,0 +1,29 @@
+--livery by White Crow
+livery = 
+{
+  
+    {"e2d_fus", 0, "e2d_fus", false};
+    {"e2d_fus", 1, "e2d_fus_nrm", true};
+    {"e2d_fus", 5, "f15_damage_main", true};
+    {"e2d_wing", 0, "e2d_wing", false};
+    {"e2d_wing", 1, "e2d_wing_nrm", true};
+    {"e2d_wing", 5, "f15_damage_main", true};
+    {"e2d_glass", 0, "e2d_glass", true};
+    {"e2d_glass", 5, "f15_damage_main", true};
+    {"e2d_illum", 0, "e2d_glass", true};
+    {"e2d_illum", 8, "e2d_glass", true};
+    {"e2d_BANO_bill", 0, "f15_bano", true};
+    {"f15numbers", 0, "e2d_numbers", true};
+    {"e2d_prop", 0, "e2d_prop", true};
+    {"e2d_headlight", 0, "f15_bano", true};
+    {"e2d_headlight", 8, "f15_bano", true};
+    {"e2d_pilot_helmet", 0, "hgu55", true};
+    {"e2d_pilot", 0, "pilot_f15_00", true};
+    {"e2d_wso", 0, "pilot_f15_01", true};
+    {"e2d_glass_pbr", 0, "e2d_glass_refl", true};
+    {"e2d_glass_pbr", 5, "f15_damage_main", true};
+    {"e2d_glass_pbr", 14, "e2d_glass_dif", true};
+}
+
+name = "CVW-69 E-2C Hawkeye"
+
---
+++ CoreMods/tech/USS_Nimitz/Liveries/cvn_71/USS Danny DeVito/description.lua
@@ -0,0 +1,8 @@
+livery = { 
+	{"Nimitz_Bort_Back", DECAL, "nimitz_decals", false};
+	{"Nimitz_Bort_Front_Decal", DECAL , "nimitz_decals", false};
+	{"Island_Decal", DECAL , "nimitz_decals", true};
+	{"Flag_Mat",	DIFFUSE			,	"flag_usa_base", true};
+	
+
+}
